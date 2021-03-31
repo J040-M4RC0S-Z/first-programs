ba1d7be407dd723309973dd0d01dc40afbadf4a9
João Marcos Oliveira <73258473+J040-M4RC0S-Z@users.noreply.github.com>
2021-03-30 21:48:43 -0300

    Jogo de adivinhação em Python
    Jogo criado por mim em que o jogador tenta adivinhar um número entre 0 a 10 escolhido pelo computador. Divirta-se
     :smile:

---
+++ Adivinhação/Adivinhação.py
@@ -0,0 +1,17 @@
+from random import randint
+escolha_computador = randint(0, 10)
+print("-=-"*30)
+print("O Computador escolheu um número de 0 a 10, será que você consegue adivinhar qual é ?")
+num = int(input("Digite um palpite entre 0 e 10: "))
+cont = 1
+while num != escolha_computador:
+    print("Hmmm... Você errou :(")
+    if num < escolha_computador:
+        print("Dica: Chute um número maior !")
+    if num > escolha_computador: 
+        print("Dica: Chute um número menor !")
+    num = int(input("Dê outro palpite: "))
+    print("-=-"*10)
+    cont += 1 
+print(f"Parabéns ! Você acertou com {cont} tentativas !")
+print(f"Número escolhido pelo computador: {escolha_computador}")
---
+++ Adivinhação/REAME.md
@@ -0,0 +1,5 @@
+<h1> JOGO DE ADIVINHAÇÃO MUITO DIVERTIDO COM PYTHON</h1>
+<p> Veja o meu código de um jogo em que voc~~e tenta adivinhar um número entre 0 a 10 que o computador escolheu. Ao trminar, ele te diz quantas tentativas você precisou para acertar o número escolhido pelo computador</p>
+<section>
+<footer>DIVIRTA-SE COM ESTE PROGRAMA</footer>
+</section>
