380af382d0139eba4b13688e78effbd1c21b3213
João Marcos Oliveira <73258473+J040-M4RC0S-Z@users.noreply.github.com>
2021-03-30 21:49:29 -0300

    Update Adivinhação.py

--- Adivinhação/Adivinhação.py
+++ Adivinhação/Adivinhação.py
@@ -3,6 +3,7 @@ escolha_computador = randint(0, 10)
 print("-=-"*30)
 print("O Computador escolheu um número de 0 a 10, será que você consegue adivinhar qual é ?")
 num = int(input("Digite um palpite entre 0 e 10: "))
+print("-=-"*30)
 cont = 1
 while num != escolha_computador:
     print("Hmmm... Você errou :(")
